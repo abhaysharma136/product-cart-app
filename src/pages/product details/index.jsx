cd77867bfa82827135628fe532e74e71fe807b64
Abhay Sharma <abhaysharmajr@gmail.com>
2024-09-29 16:30:29 +0530

    made some ui changes

--- src/pages/product details/index.jsx
+++ src/pages/product details/index.jsx
@@ -1,7 +1,7 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, updateQuantity } from "../../redux/cartSlice";
-import { Button, Typography, CircularProgress, Grid } from "@mui/material";
+import { Button, Typography, CircularProgress } from "@mui/material";
 import products from "../../products";
 import { useEffect, useState } from "react";
 import styles from "./productDetails.module.css";
@@ -44,46 +44,34 @@ const ProductDetails = () => {
 
   if (loading) {
     return (
-      <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-      >
+      <div className={styles.loaderContainer}>
         <CircularProgress /> {/* Show loader while loading */}
       </div>
     );
   }
 
   return (
-    <div className={styles.mainContiner}>
+    <div className={styles.mainContainer}>
       <div>
-        <Grid
-          container
-          spacing={2}
-          sx={{ display: "flex", justifyContent: "center" }}
-        >
-          <Grid item xs={12} md={4}>
-            <div className={styles.productImageContainer}>
-              <img
-                src={product.image}
-                alt={product.name}
-                style={{ width: "60%", borderRadius: 25 }}
-                className={styles.productImage}
-              />
-            </div>
-          </Grid>
-
-          <Grid item xs={12} md={5}>
-            <div className={styles.rightDetailsContainer}>
-              <Typography variant="h5" gutterBottom>
-                {product.name}
-              </Typography>
-              <Typography ariant="h5" gutterBottom>
-                Geeru gundu badenekaayi
-              </Typography>
-              <div className={styles.priceContainer}>
-                <div className={styles.priceValueContainer}>
+        <div className={styles.innerMainContainer}>
+          <div className={styles.productImageContainer}>
+            <img
+              src={product.image}
+              alt={product.name}
+              className={styles.productImage}
+            />
+          </div>
+          <div className={styles.productDetailsContainer}>
+            <h6 className={styles.breadcrumb}>Home / vegetables / onions</h6>
+            <h3 className={styles.productName}>{product.name}</h3>
+            <h4 className={styles.productSubtitle}>Geeru gundu badenekaayi</h4>
+            <div className={styles.priceContainer}>
+              <div className={styles.priceValueContainer}>
+                <div className={styles.buttonPriceContainer}>
                   <Typography variant="h6" fontSize="14px">
                     1 KG
                   </Typography>
+
                   <Typography variant="h6" fontSize="14px">
                     ₹ {product.price}
                   </Typography>
@@ -92,49 +80,46 @@ const ProductDetails = () => {
                   variant="contained"
                   color="primary"
                   onClick={handleCheckout}
-                  className={styles.AddToCartButton}
+                  className={styles.addToCartButton}
                   endIcon={<AddIcon />}
-                  sx={{
-                    marginTop: "10px",
-                    borderRadius: 10,
-                    border: "none",
-                    color: "black",
-                    padding: "10px 15px",
-                    transition: "all 0.3s ease", // Smooth transition for hover effects
-                    "&:hover": {
-                      backgroundColor: "#333", // Darker shade of black for hover
-                      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.3)", // Subtle shadow effect
-                      transform: "scale(1.05)", // Slight scale up effect
-                    },
-                  }}
                 >
                   Add
                 </Button>
               </div>
-              <div className={styles.descriptionContianer}>
-                <h3 style={{ marginTop: "0", marginBottom: "5px" }}>About</h3>
-                <Typography variant="body1" color="textSecondary" paragraph>
-                  {product.description}
-                </Typography>
+            </div>
+            <div className={styles.descriptionContainer}>
+              <h3>About</h3>
+              <Typography variant="body1" color="textSecondary" paragraph>
+                {product.description}
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className={styles.storingInfoContainer}>
+          <div className={styles.storingInfoDetails}>
+            <h3>Storing Information</h3>
+            <div className={styles.storingInfoItem}>
+              <div className={styles.storingInfoCard}>
+                <div>
+                  <AddIcon />
+                </div>
+                <div>
+                  <div>10 Days</div>
+                  <div>Room temp. 19-2</div>
+                </div>
+              </div>
+              <div className={styles.storingInfoCard}>
+                <div>
+                  <AddIcon />
+                </div>
+                <div>
+                  <div>3 Weeks</div>
+                  <div>Refrigerate 16-19 C</div>
+                </div>
               </div>
             </div>
-          </Grid>
-        </Grid>
-      </div>
-      <div style={{ width: "75%" }} className={styles.storingDetailsContainer}>
-        <Grid sx={{ display: "flex" }}>
-          <Grid style={{ background: "grey" }} xs={12} md={5}>
-            <h3>Storing informartion</h3>
-            <div>
-              <div>10 Days</div>
-              <div>Room temp. 19-2</div>
-            </div>
-            <div>
-              <div>3 Weeks</div>
-              <div>Refrigrate 16-19 C</div>
-            </div>
-          </Grid>
-        </Grid>
+          </div>
+        </div>
       </div>
     </div>
   );
